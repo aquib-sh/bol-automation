3bcea70d2d77864511692102394a56f71e623066
aquib-sh <shaikhaquib394@gmail.com>
2022-04-12 16:19:18 +0530

    Add TokenRetreiver module

---
+++ token.py
@@ -0,0 +1,21 @@
+import json
+
+
+class TokenRetriever:
+    def __init__(self, token_file):
+        self.token_file = token_file 
+        self.__api_id = None
+        self.__api_secret = None
+        self.__retrieve()
+
+    def __retrieve(self):
+        fp = open(self.token_file, "r")
+        data = json.loads(fp.read())
+        self.__api_id = data['API_ID']
+        self.__api_secret = data['API_SECRET']
+    
+    def get_api_id(self):
+        return self.__api_id
+
+    def get_api_secret(self):
+        return self.__api_secret
